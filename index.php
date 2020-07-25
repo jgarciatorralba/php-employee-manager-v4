32155d4a77b36e0a66b904c175c0bfe461d9b0e6
Christian Callau <ch.callau@gmail.com>
2020-07-25 18:21:56 +0200

    provisional header and footer

--- index.php
+++ index.php
@@ -11,14 +11,20 @@
 </head>
 
 <body>
-    <form class="form-signin text-center mt-5">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputName" class="sr-only">Username</label>
-        <input type="text" id="inputName" class="form-control" placeholder="Username" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control mb-3" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>
+    <?php include('assets/header.html') ?>
+
+    <div class="container">
+        <form class="form-signin text-center mt-5">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputName" class="sr-only">Username</label>
+            <input type="text" id="inputName" class="form-control" placeholder="Username" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control mb-3" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+        <?php include('assets/footer.html') ?>
+    </div>
+
 
     <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
     <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
