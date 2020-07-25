2540bf97b8fd5905a62a10de46a448f8864f0559
Christian Callau <ch.callau@gmail.com>
2020-07-25 06:18:14 +0200

    added dependencies links to index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<!-- TODO Application entry point. Login view -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+</head>
+
+<body>
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
