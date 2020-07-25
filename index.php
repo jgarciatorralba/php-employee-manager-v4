26930c81cc1995d25769a7e3f18b8406af36c359
Christian Callau <ch.callau@gmail.com>
2020-07-25 06:40:43 +0200

    basic bootstrap login form

--- index.php
+++ index.php
@@ -4,10 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Sign In</title>
 </head>
 
 <body>
+    <form class="form-signin text-center mt-5">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputName" class="sr-only">Username</label>
+        <input type="text" id="inputName" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control mb-3" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+
     <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
     <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
