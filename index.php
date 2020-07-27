99fd6f3430fe5c94790b72af2c88cebf8d367e03
Christian Callau <ch.callau@gmail.com>
2020-07-27 21:44:22 +0200

    moved css folder into assets folder

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ if (isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="assets/css/login.css">
     <title>Log in</title>
 </head>
 
