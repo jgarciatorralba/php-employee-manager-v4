0eb040fa87138a73dfad9e7006bdc7ffdd5f3862
Christian Callau <ch.callau@gmail.com>
2020-07-28 01:28:47 +0200

    removed repeated line

--- src/library/sessionHelper.php
+++ src/library/sessionHelper.php
@@ -7,8 +7,6 @@ session_start();
 if (!activeSession()) header('Location: ../index.php');
 else if (sessionTimeout()) logout();
 
-if (!activeSession()) header('Location: ../index.php');
-
 function activeSession()
 {
     return isset($_SESSION['username']);
