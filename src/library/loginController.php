e6f09efd4ca51d4cf4c1c643509393b36bc27dff
Christian Callau <ch.callau@gmail.com>
2020-07-27 20:31:19 +0200

    logout action

--- src/library/loginController.php
+++ src/library/loginController.php
@@ -4,20 +4,24 @@ include('loginManager.php');
 
 session_start();
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    $_SESSION = array();
+    session_destroy();
+    header('Location: ../../index.php');
+}
 
+if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
     unset($_POST['username']);
     unset($_POST['password']);
 
     if (checkCredentials($username, $password)) {
         $_SESSION['username'] = $username;
         $_SESSION['password'] = $password;
-        header("Location: ../dashboard.php");
+        header('Location: ../dashboard.php');
     } else {
         session_destroy();
-        header("Location: ../../index.php");
+        header('Location: ../../index.php');
     }
 }
