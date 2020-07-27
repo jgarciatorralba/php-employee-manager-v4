23f051935989265b775519ecb96fe3b8710c52aa
Christian Callau <ch.callau@gmail.com>
2020-07-27 20:13:57 +0200

    login manager refactor

--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -3,7 +3,14 @@
 function checkCredentials($username, $password)
 {
     $user = getUser($username);
-    return password_verify($password, $user->password);
+    return $user ? password_verify($password, $user->password) : false;
+}
+
+function getUser($username)
+{
+    $users = getUsers();
+    $key = array_search($username, array_column($users, 'name'));
+    return is_numeric($key) ? $users[$key] : false;
 }
 
 function getUsers()
@@ -11,12 +18,4 @@ function getUsers()
     $path = '../../resources/users.json';
     $data = file_get_contents($path);
     return json_decode($data)->users;
-}
-
-function getUser($username)
-{
-    $users = getUsers();
-    $key = array_search($username, array_column($users, 'name'));
-    if (is_numeric($key)) return $users[$key];
-    return false;
 }
