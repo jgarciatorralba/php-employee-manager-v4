3f98e1ab668a245983372ec4b9adcf0d27f34404
Christian Callau <ch.callau@gmail.com>
2020-07-28 02:45:38 +0200

    dashboard refacor

--- src/dashboard.php
+++ src/dashboard.php
@@ -6,8 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="../node_modules/jsgrid/dist/jsgrid.min.css">
-    <link type="text/css" rel="stylesheet" href="../node_modules/jsgrid/dist/jsgrid-theme.min.css">
+    <link rel="stylesheet" href="../node_modules/jsgrid/dist/jsgrid.min.css">
+    <link rel="stylesheet" href="../node_modules/jsgrid/dist/jsgrid-theme.min.css">
     <link rel="stylesheet" href="../css/main.css">
     <title>Dashboard</title>
 </head>
@@ -26,7 +26,7 @@
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
+    <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
     <script src="../assets/js/dashboard.js"></script>
 </body>
 
