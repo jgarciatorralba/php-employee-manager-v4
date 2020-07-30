6d79e54b041fd3a86c7bb80d06666ecf1b013686
Christian Callau <ch.callau@gmail.com>
2020-07-31 01:09:44 +0200

    employee controller refactor

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -9,12 +9,12 @@ header('Content-Type: application/json');
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         $data = getHandler();
-        if ($data) echo $data;
+        if ($data) echo json_encode($data);
         else http_response_code(404);
         break;
     case 'POST':
         $data = postHandler();
-        if ($data) echo $data;
+        if ($data) echo json_encode($data);
         else http_response_code(500);
         break;
     case 'DELETE':
@@ -27,25 +27,21 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
 function getHandler()
 {
-    if (isset($_GET['employee'])) $data = getEmployee($_GET['employee']);
-    else $data = readEmployees();
-    return $data ? json_encode($data) : false;
+    return isset($_GET['employee']) ? getEmployee($_GET['employee']) : readEmployees();
 }
 
 function postHandler()
 {
     $employee = $_POST;
     if (count($employee) === 0) return false;
-    $result = isset($employee['id']) && is_numeric($employee['id']) ? updateEmployee($employee) : addEmployee($employee);
     // propiety redirect is set if post request is from employee.php
     if (isset($employee['redirect'])) header('Location: ../employee.php?id=' . $employee['id'] . '&success=true');
-    return $result ? json_encode($result) : false;
+    return isset($employee['id']) && is_numeric($employee['id']) ? updateEmployee($employee) : addEmployee($employee);
 }
 
 function deleteHandler()
 {
     parse_str(file_get_contents("php://input"), $data);
     if (!isset($data['id'])) return false;
-    else header("Location: ./index.php");
     return deleteEmployee($data['id']);
 }
