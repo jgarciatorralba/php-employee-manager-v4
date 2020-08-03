e51d3a3d40906e5d5c68aaa76b13c9cb6c0d9a7c
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-03 22:52:00 +0200

    Added 404 error page

---
+++ view/error.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="row justify-content-center align-items-center form-cont">
+    <div class="text-center p-5 bg-danger border border-dark rounded-lg">
+      <img id="error_404" src="assets/img/404_error.png" alt="Error 404 not found">
+      <h2>Not Found</h2>
+      <h6>The resource requested could not be found on this server!</h6>
+    </div>
+  </div>
+</div>
