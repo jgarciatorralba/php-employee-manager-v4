9ec2c20341b401d83398bc1e27ca0c66c16c3d9a
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-04 13:44:40 +0200

    modified jsGrid ajax request

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,56 +1,110 @@
-$('#jsGrid').jsGrid({
-    width: '100%',
+$.get("resources/employees.json", function (data) {
+    $("#jsGrid").jsGrid({
+        width: '100%',
 
-    filtering: false,
-    inserting: true,
-    editing: false,
-    sorting: true,
-    paging: true,
-    autoload: true,
-    confirmDeleting: true,
-    deleteConfirm: 'Are you sure?',
+        filtering: false,
+        inserting: true,
+        editing: false,
+        sorting: true,
+        paging: true,
+        autoload: true,
+        confirmDeleting: true,
+        deleteConfirm: 'Are you sure?',
 
-    pageSize: 10,
-    pageButtonCount: 5,
+        data: data,
 
-    align: 'center',
+        pageSize: 10,
+        pageButtonCount: 5,
 
-    controller: {
-        loadData: function () {
-            return $.ajax('controller/employee.php').done(data => console.log(data));
+        align: 'center',
+
+        controller: {
+            // loadData: function () {
+            //     return $.ajax('index.php?controller=employee.php').done(
+            //         data => {
+            //             JSON.parse(data);
+            //             console.log(data);
+            //         }
+            //     );
+            // },
+            insertItem: function (item) {
+                return $.ajax({
+                    url: 'controller/employee.php',
+                    method: 'POST',
+                    data: item
+                }).done(response => {
+                    return response;
+                }).fail(console.log);
+            },
+            deleteItem: function (item) {
+                return $.ajax({
+                    url: 'controller/employee.php',
+                    method: 'DELETE',
+                    data: {
+                        id: item.id
+                    }
+                }).fail(console.log);
+            }
         },
-        insertItem: function (item) {
-            return $.ajax({
-                url: 'controller/employee.php',
-                method: 'POST',
-                data: item
-            }).done(response => {
-                return response;
-            }).fail(console.log);
+
+        rowClick: function (args) {
+            location.href = `index.php?controller=employee.php&action=getEmployee&id=${args.item.id}`;
         },
-        deleteItem: function (item) {
-            return $.ajax({
-                url: 'controller/employee.php',
-                method: 'DELETE',
-                data: { id: item.id }
-            }).fail(console.log);
-        }
-    },
 
-    rowClick: function (args) {
-        location.href = `index.php?employee&id=${args.item.id}`;
-    },
-
-    fields: [
-        { name: 'id', type: 'number', visible: false },
-        { name: 'name', type: 'text', title: 'Name', width: 80 },
-        { name: 'email', type: 'text', title: 'Email' },
-        { name: 'age', type: 'number', title: 'Age', width: 40 },
-        { name: 'streetAddress', type: 'text', title: 'Street No.', width: 80 },
-        { name: 'city', type: 'text', title: 'City', },
-        { name: 'state', type: 'text', title: 'State', width: 40 },
-        { name: 'postalCode', type: 'text', title: 'Postal Code', width: 80 },
-        { name: 'phoneNumber', type: 'text', title: 'Phone Number', },
-        { type: 'control', editButton: false }
-    ]
+        fields: [{
+                name: 'id',
+                type: 'number',
+                visible: false
+            },
+            {
+                name: 'name',
+                type: 'text',
+                title: 'Name',
+                width: 80
+            },
+            {
+                name: 'email',
+                type: 'text',
+                title: 'Email'
+            },
+            {
+                name: 'age',
+                type: 'number',
+                title: 'Age',
+                width: 40
+            },
+            {
+                name: 'streetAddress',
+                type: 'text',
+                title: 'Street No.',
+                width: 80
+            },
+            {
+                name: 'city',
+                type: 'text',
+                title: 'City',
+            },
+            {
+                name: 'state',
+                type: 'text',
+                title: 'State',
+                width: 40
+            },
+            {
+                name: 'postalCode',
+                type: 'text',
+                title: 'Postal Code',
+                width: 80
+            },
+            {
+                name: 'phoneNumber',
+                type: 'text',
+                title: 'Phone Number',
+            },
+            {
+                type: 'control',
+                editButton: false
+            }
+        ]
+    })
 });
