b9576232440561dee85a147e01feff4c08d05305
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-07 15:29:01 +0200

    Wrapped content into a callable function

--- lib/database.php
+++ lib/database.php
@@ -1,18 +1,15 @@
 <?php
   // This file will contain the function to create the connection with the database. Keep in mind that you must use your previously created database constants in order to connect with your database.
 
-  require_once "../config/db.php";
-
-  $servername = HOST;
-  $database = DATABASE;
-  $username = USER;
-  $password = PASSWORD;
-
+function setConnection(string $servername, string $database, string $username, string $password)
+{
   try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    // echo "Connected successfully";
+    return $conn;
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
+}
