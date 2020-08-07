a84d96aa1c84ce9d8d21f29ee0cb8b380cfaf202
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-07 14:40:01 +0200

    Changed DB name to use to "employeeManagement"

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
   // Define constants that contain the required information to create the connection with the database.
   define ("HOST", 'localhost');
-  define ("DATABASE", 'employees');
+  define ("DATABASE", 'employeeManagement');
   define ("USER", 'root');
   define ("PASSWORD", '');
