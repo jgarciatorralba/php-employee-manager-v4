a535437737db194c5445b26736e0fb758068bd5e
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-07 17:19:39 +0200

    Replaced URL to get employee list from controller

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,113 +1,106 @@
-// $.get("resources/employees.json", function (data) {
-$("#jsGrid").jsGrid({
-    width: '100%',
+$.get("index.php?controller=employee", function (data) {
+    $("#jsGrid").jsGrid({
+        width: '100%',
 
-    filtering: false,
-    inserting: true,
-    editing: false,
-    sorting: true,
-    paging: true,
-    autoload: true,
-    confirmDeleting: true,
-    deleteConfirm: 'Are you sure?',
+        filtering: false,
+        inserting: true,
+        editing: false,
+        sorting: true,
+        paging: true,
+        autoload: true,
+        confirmDeleting: true,
+        deleteConfirm: 'Are you sure?',
 
-    // data: data,
-    // data: JSON.parse(data),
+        // data: data,
+        data: JSON.parse(data),
 
-    pageSize: 10,
-    pageButtonCount: 5,
+        pageSize: 10,
+        pageButtonCount: 5,
 
-    align: 'center',
+        align: 'center',
 
-    controller: {
-        loadData: function () {
-            return $.ajax({
-                url: 'index.php?controller=employee',
-                method: 'GET'
-            }).done(response => {
-                // return response;
-                console.log(JSON.parse(response));
-                return JSON.parse(response);
-            }).fail(console.log);
+        controller: {
+            // loadData: function () {
+            //     return $.ajax('library/employeeController.php');
+            // },
+            insertItem: function (item) {
+                return $.ajax({
+                    url: 'index.php?controller=employee&action=createEmployeeAJAX',
+                    method: 'POST',
+                    data: item
+                }).done(response => {
+                    return response;
+                }).fail(console.log);
+            },
+            deleteItem: function (item) {
+                return $.ajax({
+                    url: 'index.php?controller=employee&action=deleteEmployeeAJAX',
+                    method: 'DELETE',
+                    data: {
+                        id: item.id
+                    }
+                }).fail(console.log);
+            }
         },
-        insertItem: function (item) {
-            return $.ajax({
-                url: 'index.php?controller=employee&action=createEmployeeAJAX',
-                method: 'POST',
-                data: item
-            }).done(response => {
-                return response;
-            }).fail(console.log);
-        },
-        deleteItem: function (item) {
-            return $.ajax({
-                url: 'index.php?controller=employee&action=deleteEmployeeAJAX',
-                method: 'DELETE',
-                data: {
-                    id: item.id
-                }
-            }).fail(console.log);
-        }
-    },
 
-    rowClick: function (args) {
-        location.href = `index.php?controller=employee&action=showEmployeeForm&id=${args.item.id}`;
-    },
+        rowClick: function (args) {
+            location.href = `index.php?controller=employee&action=showEmployeeForm&id=${args.item.id}`;
+        },
 
-    fields: [{
-            name: 'id',
-            type: 'number',
-            visible: false
-        },
-        {
-            name: 'name',
-            type: 'text',
-            title: 'Name',
-            width: 80
-        },
-        {
-            name: 'email',
-            type: 'text',
-            title: 'Email'
-        },
-        {
-            name: 'age',
-            type: 'number',
-            title: 'Age',
-            width: 40
-        },
-        {
-            name: 'streetAddress',
-            type: 'text',
-            title: 'Street No.',
-            width: 80
-        },
-        {
-            name: 'city',
-            type: 'text',
-            title: 'City',
-        },
-        {
-            name: 'state',
-            type: 'text',
-            title: 'State',
-            width: 40
-        },
-        {
-            name: 'postalCode',
-            type: 'text',
-            title: 'Postal Code',
-            width: 80
-        },
-        {
-            name: 'phoneNumber',
-            type: 'text',
-            title: 'Phone Number',
-        },
-        {
-            type: 'control',
-            editButton: false
-        }
-    ]
+        fields: [{
+                name: 'id',
+                type: 'number',
+                visible: false
+            },
+            {
+                name: 'name',
+                type: 'text',
+                title: 'Name',
+                width: 80
+            },
+            {
+                name: 'email',
+                type: 'text',
+                title: 'Email'
+            },
+            {
+                name: 'age',
+                type: 'number',
+                title: 'Age',
+                width: 40
+            },
+            {
+                name: 'streetAddress',
+                type: 'text',
+                title: 'Street No.',
+                width: 80
+            },
+            {
+                name: 'city',
+                type: 'text',
+                title: 'City',
+            },
+            {
+                name: 'state',
+                type: 'text',
+                title: 'State',
+                width: 40
+            },
+            {
+                name: 'postalCode',
+                type: 'text',
+                title: 'Postal Code',
+                width: 80
+            },
+            {
+                name: 'phoneNumber',
+                type: 'text',
+                title: 'Phone Number',
+            },
+            {
+                type: 'control',
+                editButton: false
+            }
+        ]
+    });
 });
-// });
