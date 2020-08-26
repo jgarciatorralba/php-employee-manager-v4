210ebfa1992b0c9b9596893376bac3f4c9fc9fd7
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 12:09:18 +0200

    Moved routing from index.php to app.php

--- index.php
+++ index.php
@@ -2,25 +2,9 @@
 require_once "config/config.php";
 require_once "config/db.php";
 
-// require_once LIB."sessionHelper.php";
+require_once LIB."sessionHelper.php";
 
-// if (!isset($_GET["controller"])){
-//     if (activeSession()) {
-//         include(VIEW . 'dashboard.php');
-//     } else {
-//         include(VIEW . 'login.php');
-//     }
 
-// } else {
-    
-//     $controller = $_GET["controller"];
-//     if (file_exists(CONTROLLER . $controller . ".php")) {
-//         include(CONTROLLER . $controller . ".php");
-//     } else {
-//         include(VIEW . "error.php");
-//     }
-
-// }
 
 // Trying to convert to OOP
 
--- lib/app.php
+++ lib/app.php
@@ -4,7 +4,21 @@
     
     function __construct()
     {
-      echo "<p>Nueva App</p>";
-    }
+      // echo "<p>New App</p>";
 
+      if (!isset($_GET["controller"])){
+        if (activeSession()) {
+            include(VIEW . 'dashboard.php');
+        } else {
+            include(VIEW . 'login.php');
+        }
+      } else {
+        $controller = $_GET["controller"];
+        if (!file_exists(CONTROLLER . $controller . ".php")) {
+          include(VIEW . "error.php");
+        } else {
+          include(CONTROLLER . $controller . ".php");
+        }
+      }
+    }
   }
