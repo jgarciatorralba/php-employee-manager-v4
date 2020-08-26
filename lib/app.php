6f3a3cb5eb16ec295a4f35611e888c86a5182bab
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 16:09:03 +0200

    Added comment to explain functioning of the class

--- lib/app.php
+++ lib/app.php
@@ -1,11 +1,15 @@
 <?php
 
+  /*
+    This class will handle the routing of the web application, 
+    switching between controllers, allowing views ("login", "dashboard" or "error") 
+    if none or an invalid controller is set, or executing controller actions.
+  */
+
   class App {
-    
+
     function __construct()
     {
-      // echo "<p>New App</p>";
-
       if (!isset($_GET["controller"])){
         if (activeSession()) {
             include(VIEW . 'dashboard.php');
@@ -13,12 +17,30 @@
             include(VIEW . 'login.php');
         }
       } else {
-        $controller = $_GET["controller"];
-        if (!file_exists(CONTROLLER . $controller . ".php")) {
+        $controllerName = $_GET["controller"];
+        if (!file_exists(CONTROLLER . $controllerName . ".php")) {
           include(VIEW . "error.php");
         } else {
-          include(CONTROLLER . $controller . ".php");
+          include(CONTROLLER . $controllerName . ".php");
+          $className = $controllerName . "Controller";
+          $controller = new $className;
+
+          $action;
+          if (isset($_REQUEST["action"])) {
+            $action = $_REQUEST["action"];
+          } else {
+            if ($controllerName == "login"){
+              $action = "goToLogin";
+            } else if ($controllerName == "employee"){
+              $action = "showEmployees";
+            }
+          }
+
+          if (method_exists($controller, $action)) {
+            $controller->{$action}();
+          }
         }
       }
     }
+
   }
