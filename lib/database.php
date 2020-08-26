b937dbfc7701467f6014968148e463bc57db9db4
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 22:51:16 +0200

    Re-factoring of file 'database.php' to apply OOP

--- lib/database.php
+++ lib/database.php
@@ -1,15 +1,22 @@
 <?php
-  // This file will contain the function to create the connection with the database. Keep in mind that you must use your previously created database constants in order to connect with your database.
+  /*
+    This file will contain a class with the method responsible to create the connection with the database. 
+    Keep in mind that you must use your previously created database constants in order to connect with your database.
+  */
 
-function setConnection(string $servername, string $database, string $username, string $password)
-{
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
-    return $conn;
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+  class Database {
+
+    public static function setConnection(string $servername, string $database, string $username, string $password)
+    {
+      try {
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully";
+        return $conn;
+      } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
+    }
+
   }
-}
