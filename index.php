a9983e9e2533ab9978ba60eb5abaf6db1c7c3e5d
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 15:54:46 +0200

    Re-factored file 'index.php'

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-require_once "config/config.php";
-require_once "config/db.php";
+    // Require constants to use in file paths and database connections.
+    require_once "config/config.php";
+    require_once "config/db.php";
 
-require_once LIB."sessionHelper.php";
+    // Require auxiliar file to check session timeout.
+    require_once LIB."sessionHelper.php";
 
-
-
-// Trying to convert to OOP
-
-require_once LIB."app.php";
-
-$app = new App();
+    // Instantiate new "App" object to handle the web application.
+    require_once LIB."app.php";
+    $app = new App();
+    
