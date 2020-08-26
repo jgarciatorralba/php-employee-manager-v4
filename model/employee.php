1cb509d776c63b380b81a9ca4696b085970b9ddf
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 16:15:00 +0200

    Added indentation to model files

--- model/employee.php
+++ model/employee.php
@@ -1,152 +1,152 @@
 <?php
 
-include_once(LIB."database.php");
+    include_once(LIB."database.php");
 
-function addEmployee(array $newEmployee)
-{
-    $employees = readEmployees();
-    $newEmployee['id'] = getNextIdentifier($employees);
+    function addEmployee(array $newEmployee)
+    {
+        $employees = readEmployees();
+        $newEmployee['id'] = getNextIdentifier($employees);
 
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-    if ($conn) {
-        $id = $newEmployee['id'];
-        if (isset($newEmployee['avatar'])){
-            $avatar = $newEmployee['avatar'];
-        } else {
-            $avatar = "";
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+        if ($conn) {
+            $id = $newEmployee['id'];
+            if (isset($newEmployee['avatar'])){
+                $avatar = $newEmployee['avatar'];
+            } else {
+                $avatar = "";
+            }
+            $name = $newEmployee['name'];
+            if (isset($newEmployee['lastName'])){
+                $lastName = $newEmployee['lastName'];
+            } else {
+                $lastName = "";
+            }
+            $email = $newEmployee['email'];
+            if (isset($newEmployee['gender'])){
+                $gender = $newEmployee['gender'];
+            } else {
+                $gender = "";
+            }
+            $city = $newEmployee['city'];
+            $streetAddress = $newEmployee['streetAddress'];
+            $state = $newEmployee['state'];
+            $parsedAge = intval($newEmployee['age']);
+            $postalCode = $newEmployee['postalCode'];
+            $phoneNumber = $newEmployee['phoneNumber'];
+
+            $sql =
+                "INSERT INTO 
+                    employees (id, avatar, name, lastName, email, gender, city, streetAddress, state, age, postalCode, phoneNumber) 
+                    values ($id, '$avatar', '$name', '$lastName', '$email', '$gender', '$city', '$streetAddress', '$state', $parsedAge, $postalCode, $phoneNumber)"
+            ; 
+            $conn->prepare($sql)->execute();
+
+            // close connection
+            $conn = null;
         }
-        $name = $newEmployee['name'];
-        if (isset($newEmployee['lastName'])){
-            $lastName = $newEmployee['lastName'];
-        } else {
-            $lastName = "";
+    }
+
+
+    function deleteEmployee(string $id)
+    {
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+        if ($conn) {
+            $parsedId = intval($id);
+            $stmt = $conn->prepare("DELETE FROM employees WHERE id = $parsedId;");
+            $stmt->execute();
+
+            // close connection
+            $conn = null;
         }
-        $email = $newEmployee['email'];
-        if (isset($newEmployee['gender'])){
-            $gender = $newEmployee['gender'];
-        } else {
-            $gender = "";
+    }
+
+
+    function updateEmployee(array $updateEmployee)
+    {
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+
+        if ($conn) {
+            $parsedId = intval($updateEmployee['id']);
+            $redirect = $updateEmployee['redirect'];
+            $avatar = $updateEmployee['avatar'];
+            $name = $updateEmployee['name'];
+            $lastName = $updateEmployee['lastName'];
+            $email = $updateEmployee['email'];
+            $gender = $updateEmployee['gender'];
+            $city = $updateEmployee['city'];
+            $streetAddress = $updateEmployee['streetAddress'];
+            $state = $updateEmployee['state'];
+            $parsedAge = intval($updateEmployee['age']);
+            $postalCode = $updateEmployee['postalCode'];
+            $phoneNumber = $updateEmployee['phoneNumber'];
+
+            $sql = 
+                "UPDATE employees 
+                    SET 
+                        redirect = '$redirect', 
+                        avatar = '$avatar', 
+                        name = '$name', 
+                        lastName = '$lastName', 
+                        email = '$email', 
+                        gender = '$gender', 
+                        city = '$city', 
+                        streetAddress = '$streetAddress', 
+                        state = '$state', 
+                        age = $parsedAge, 
+                        postalCode = $postalCode, 
+                        phoneNumber = $phoneNumber 
+                    WHERE id = $parsedId;"
+            ;
+            $conn->exec($sql);
+
+            // close connection
+            $conn = null;
         }
-        $city = $newEmployee['city'];
-        $streetAddress = $newEmployee['streetAddress'];
-        $state = $newEmployee['state'];
-        $parsedAge = intval($newEmployee['age']);
-        $postalCode = $newEmployee['postalCode'];
-        $phoneNumber = $newEmployee['phoneNumber'];
-
-        $sql =
-            "INSERT INTO 
-                employees (id, avatar, name, lastName, email, gender, city, streetAddress, state, age, postalCode, phoneNumber) 
-                values ($id, '$avatar', '$name', '$lastName', '$email', '$gender', '$city', '$streetAddress', '$state', $parsedAge, $postalCode, $phoneNumber)"
-        ; 
-        $conn->prepare($sql)->execute();
-
-        // close connection
-        $conn = null;
     }
-}
 
 
-function deleteEmployee(string $id)
-{
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-    if ($conn) {
-        $parsedId = intval($id);
-        $stmt = $conn->prepare("DELETE FROM employees WHERE id = $parsedId;");
-        $stmt->execute();
+    function getEmployee(string $id)
+    {
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+        if ($conn) {
+            $parsedId = intval($id);
+            $stmt = $conn->prepare("SELECT * FROM employees WHERE id = $parsedId;");
+            $stmt->execute();
 
-        // close connection
-        $conn = null;
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+            // close connection
+            $conn = null;
+            return $result;
+        }
     }
-}
 
-
-function updateEmployee(array $updateEmployee)
-{
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-
-    if ($conn) {
-        $parsedId = intval($updateEmployee['id']);
-        $redirect = $updateEmployee['redirect'];
-        $avatar = $updateEmployee['avatar'];
-        $name = $updateEmployee['name'];
-        $lastName = $updateEmployee['lastName'];
-        $email = $updateEmployee['email'];
-        $gender = $updateEmployee['gender'];
-        $city = $updateEmployee['city'];
-        $streetAddress = $updateEmployee['streetAddress'];
-        $state = $updateEmployee['state'];
-        $parsedAge = intval($updateEmployee['age']);
-        $postalCode = $updateEmployee['postalCode'];
-        $phoneNumber = $updateEmployee['phoneNumber'];
-
-        $sql = 
-            "UPDATE employees 
-                SET 
-                    redirect = '$redirect', 
-                    avatar = '$avatar', 
-                    name = '$name', 
-                    lastName = '$lastName', 
-                    email = '$email', 
-                    gender = '$gender', 
-                    city = '$city', 
-                    streetAddress = '$streetAddress', 
-                    state = '$state', 
-                    age = $parsedAge, 
-                    postalCode = $postalCode, 
-                    phoneNumber = $phoneNumber 
-                WHERE id = $parsedId;"
-        ;
-        $conn->exec($sql);
-
-        // close connection
-        $conn = null;
+    function getQueryStringParameters(): array
+    {
+        return $_GET;
     }
-}
 
 
-function getEmployee(string $id)
-{
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-    if ($conn) {
-        $parsedId = intval($id);
-        $stmt = $conn->prepare("SELECT * FROM employees WHERE id = $parsedId;");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $stmt->fetch();
-        // close connection
-        $conn = null;
-        return $result;
+    function getNextIdentifier(array $employeesCollection): int
+    {
+        $last_id = (int) end($employeesCollection)["id"];
+        return $last_id + 1;
     }
-}
-
-function getQueryStringParameters(): array
-{
-    return $_GET;
-}
 
 
-function getNextIdentifier(array $employeesCollection): int
-{
-    $last_id = (int) end($employeesCollection)["id"];
-    return $last_id + 1;
-}
+    function readEmployees()
+    {
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+        if ($conn) {
+            $stmt = $conn->prepare("SELECT * FROM employees;");
+            $stmt->execute();
 
-
-function readEmployees()
-{
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-    if ($conn) {
-        $stmt = $conn->prepare("SELECT * FROM employees;");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $stmt->fetchAll();
-        // close connection
-        $conn = null;
-        return $result;
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+            // close connection
+            $conn = null;
+            return $result;
+        }
     }
-}
--- model/login.php
+++ model/login.php
@@ -1,48 +1,48 @@
 <?php
 
-include_once(LIB."database.php");
+    include_once(LIB."database.php");
 
-function checkCredentials($username, $password)
-{
-    $user = getUser($username);
-    return password_verify($password, $user["password"]);
-}
-
-function getUser($username)
-{
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-    if ($conn) {
-        $stmt = $conn->prepare("SELECT * FROM users WHERE name = '$username';");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $stmt->fetch();
-        // close connection
-        $conn = null;
-        return $result;
+    function checkCredentials($username, $password)
+    {
+        $user = getUser($username);
+        return password_verify($password, $user["password"]);
     }
-}
 
-function getUsers()
-{
-    $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
-    if ($conn) {
-        $stmt = $conn->prepare("SELECT * FROM users");
-        $stmt->execute();
+    function getUser($username)
+    {
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+        if ($conn) {
+            $stmt = $conn->prepare("SELECT * FROM users WHERE name = '$username';");
+            $stmt->execute();
 
-        // set the resulting array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $stmt->fetch();
-        // close connection
-        $conn = null;
-        return $result;
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+            // close connection
+            $conn = null;
+            return $result;
+        }
     }
-}
 
-function logOut()
-{
-    $_SESSION = array();
-    session_destroy();
-    header("Location: index.php");
-}
+    function getUsers()
+    {
+        $conn = setConnection (HOST, DATABASE, USER, PASSWORD);
+        if ($conn) {
+            $stmt = $conn->prepare("SELECT * FROM users");
+            $stmt->execute();
+
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+            // close connection
+            $conn = null;
+            return $result;
+        }
+    }
+
+    function logOut()
+    {
+        $_SESSION = array();
+        session_destroy();
+        header("Location: index.php");
+    }
