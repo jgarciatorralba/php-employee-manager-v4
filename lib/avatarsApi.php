c7a283c17e903674295c94bf1973ca61799d752e
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 17:24:46 +0200

    File 'avatarsAPI' re-worked to OOP

--- lib/avatarsApi.php
+++ lib/avatarsApi.php
@@ -1,19 +1,31 @@
 <?php
 
-$curl = curl_init();
+    class avatarsAPI {
 
-$API_key = "X-API-KEY: 0A2ED4BA-4E53420C-96665B70-60C6502A";
-$limit = 8;
-$emotion = 'happiness';
+        function getAvatars()
+        {
+            $curl = curl_init();
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://uifaces.co/api?limit=$limit&emotion[]=$emotion",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => array(
-        $API_key
-    ),
-));
-$output = curl_exec($curl);
-echo $output;
+            $API_key = "X-API-KEY: 0A2ED4BA-4E53420C-96665B70-60C6502A";
+            $limit = 8;
+            $emotion = 'happiness';
 
-curl_close($curl);
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://uifaces.co/api?limit=$limit&emotion[]=$emotion",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => array(
+                    $API_key
+                ),
+            ));
+            
+            $output = curl_exec($curl);
+
+            curl_close($curl);
+
+            return $output;
+        }
+
+    }
+
+    $callToAPI = new avatarsAPI();
+    echo $callToAPI->getAvatars();
