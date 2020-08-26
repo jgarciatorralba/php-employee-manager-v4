a0a2449da16d14ff24cf2904ae254ee9c058f1f9
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-26 16:13:39 +0200

    Re-worked controller files to adapt them to OOP

--- controller/employee.php
+++ controller/employee.php
@@ -1,53 +1,47 @@
 <?php
 
-require_once MODEL."employee.php";
+    require_once MODEL."employee.php";
 
-$action;
+    class EmployeeController {
 
-if (isset($_REQUEST["action"])) {
-    $action = $_REQUEST["action"];
-} else {
-    $action = "showEmployees";
-}
+        /* ~~~ CONTROLLER FUNCTIONS ~~~ */
 
-if (function_exists($action)) {
-    call_user_func($action, $_REQUEST);
-} 
+        public function showEmployees(){
+            $employees = readEmployees();
+            echo json_encode($employees);
+        }
+        
+        public function getEmployeeAJAX()
+        {
+            $employee = getEmployee($_GET["empID"]);
+            echo json_encode($employee);
+        }
+        
+        public function showEmployeeForm() {
+            include(VIEW."employee.php");
+        }
+        
+        public function createEmployeeAJAX() {
+            $employee = $_POST;
+            if (count($employee) === 0) return false;
+            return addEmployee($employee);
+        }
+        
+        public function submitEmployee()
+        {
+            $employee = $_POST;
+            if (count($employee) === 0) return false;
+            isset($employee['id']) && is_numeric($employee['id']) ? updateEmployee($employee) : addEmployee($employee);
+        
+            // propiety redirect is set if post request is from employee.php
+            if (isset($employee['redirect'])) header('Location: index.php?controller=employee&action=showEmployeeForm&id='.$employee['id'].'&success=true');
+        }
+        
+        public function deleteEmployeeAJAX()
+        {
+            parse_str(file_get_contents("php://input"), $data);
+            if (!isset($data['id'])) return false;
+            return deleteEmployee($data['id']);
+        }
 
-function showEmployees(){
-    $employees = readEmployees();
-    echo json_encode($employees);
-}
-
-function getEmployeeAJAX()
-{
-    $employee = getEmployee($_GET["empID"]);
-    echo json_encode($employee);
-}
-
-function showEmployeeForm() {
-    include(VIEW."employee.php");
-}
-
-function createEmployeeAJAX() {
-    $employee = $_POST;
-    if (count($employee) === 0) return false;
-    return addEmployee($employee);
-}
-
-function submitEmployee()
-{
-    $employee = $_POST;
-    if (count($employee) === 0) return false;    
-    isset($employee['id']) && is_numeric($employee['id']) ? updateEmployee($employee) : addEmployee($employee);
-
-    // propiety redirect is set if post request is from employee.php
-    if (isset($employee['redirect'])) header('Location: index.php?controller=employee&action=showEmployeeForm&id='.$employee['id'].'&success=true');
-}
-
-function deleteEmployeeAJAX()
-{
-    parse_str(file_get_contents("php://input"), $data);
-    if (!isset($data['id'])) return false;
-    return deleteEmployee($data['id']);
-}
+    }
--- controller/login.php
+++ controller/login.php
@@ -1,75 +1,65 @@
 <?php
 
-require_once MODEL."login.php";
+    require_once MODEL."login.php";
 
-$action;
+    class LoginController {
 
-if (isset($_REQUEST["action"])) {
-    $action = $_REQUEST["action"];
-} else {
-    $action = "goToLogin";
-}
+        /* ~~~ CONTROLLER FUNCTIONS ~~~ */
 
-if (function_exists($action)) {
-    call_user_func($action, $_REQUEST);
-} else {
-    // goToError();
-}
-
-/* ~~~ CONTROLLER FUNCTIONS ~~~ */
-
-function validateAccess()
-{
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        // unset($_POST['username']);
-        // unset($_POST['password']);
-    
-        if (checkCredentials($username, $password)) {
-            $_SESSION['username'] = $username;
-            $_SESSION['lifeTime'] = 600;
-            $_SESSION['time'] = time();
-            header('Location: index.php');
-        } else {
-            session_destroy();
-            header('Location: index.php?error');
+        public function validateAccess()
+        {
+            if (isset($_POST['username']) && isset($_POST['password'])) {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                // unset($_POST['username']);
+                // unset($_POST['password']);
+            
+                if (checkCredentials($username, $password)) {
+                    $_SESSION['username'] = $username;
+                    $_SESSION['lifeTime'] = 600;
+                    $_SESSION['time'] = time();
+                    header('Location: index.php');
+                } else {
+                    session_destroy();
+                    header('Location: index.php?error');
+                }
+                exit();
+            } else {
+                logOut();
+            }
         }
-        exit();
-    } else {
-        logOut();
+
+        public function goToDashboard()
+        {
+            if (activeSession()){
+                include(VIEW."dashboard.php");
+            } else {
+                header('Location: index.php');
+            }
+        }
+
+        public function goToEmployee()
+        {
+            if (activeSession()){
+                include(VIEW."employee.php");
+            } else {
+                header('Location: index.php');
+            }
+        }
+
+        public function goToLogin()
+        {
+            header('Location: index.php');
+        }
+
+        public function goToError()
+        {
+            include(VIEW . 'error.php');
+        }
+
+        public function kickout()
+        {
+            logOut();
+        }
+
     }
-}
-
-function goToDashboard()
-{
-    if (activeSession()){
-        include(VIEW."dashboard.php");
-    } else {
-        header('Location: index.php');
-    }
-}
-
-function goToEmployee()
-{
-    if (activeSession()){
-        include(VIEW."employee.php");
-    } else {
-        header('Location: index.php');
-    }
-}
-
-function goToLogin()
-{
-    header('Location: index.php');
-}
-
-function goToError()
-{
-    include(VIEW . 'error.php');
-}
-
-function kickout()
-{
-    logOut();
-}
