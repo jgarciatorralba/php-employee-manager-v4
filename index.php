0cc5d318e426defebd2f48b13c86c6107affe15a
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-27 15:30:03 +0200

    Added requires for MVC pattern master classes

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     require_once LIB."sessionHelper.php";
     SessionHelper::checkActiveSession();
 
+    // Require MVC pattern master classes.
+    require_once LIB."model.php";
+    require_once LIB."view.php";
+    require_once LIB."controller.php";
+
     // Instantiate new "App" object to handle the web application.
     require_once LIB."app.php";
     $app = new App();
