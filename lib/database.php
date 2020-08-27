508978897f8bf91eeb8bfd75140b22bdb3c84de7
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-27 19:25:59 +0200

    Stored constants in class private properties

--- lib/database.php
+++ lib/database.php
@@ -1,18 +1,31 @@
 <?php
   /*
-    This file will contain a class with the method responsible to create the connection with the database. 
+    This file will contain a class with the method responsible to create the connection with the database.
     Keep in mind that you must use your previously created database constants in order to connect with your database.
   */
 
   class Database {
 
-    public static function setConnection(string $servername, string $database, string $username, string $password)
+    private $host;
+    private $database;
+    private $user;
+    private $password;
+
+    public function __construct()
+    {
+      $this->host = constant('HOST');
+      $this->database = constant('DATABASE');
+      $this->user = constant('USER');
+      $this->password = constant('PASSWORD');
+    }
+
+    public function connect()
     {
       try {
         // Set DSN
-        $dsn = 'mysql:host=' . $servername . ';dbname=' . $database;
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->database;
         // Create a PDO instance
-        $conn = new PDO($dsn, $username, $password);
+        $conn = new PDO($dsn, $this->user, $this->password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
