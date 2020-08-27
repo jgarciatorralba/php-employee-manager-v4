0587bafa5f308e8db249abe859ba3478b3ad0706
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-27 19:51:41 +0200

    Added method 'loadModel' to Controller class

--- lib/controller.php
+++ lib/controller.php
@@ -7,4 +7,18 @@
       $this->view = new View();
     }
 
+    public function loadModel($model)
+    {
+      $url = MODEL . $model . '.php';
+      // echo $url;
+
+      if(file_exists($url)){
+        require $url;
+
+        $modelName = ucfirst($model) . 'Model';
+        // echo $modelName;
+        $this->model = new $modelName;
+      }
+    }
+
   }
