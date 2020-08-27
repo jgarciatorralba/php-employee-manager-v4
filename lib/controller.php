1fea27cff546513f8151cd6a3f9ac0ab4bb5cc21
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-08-27 20:09:35 +0200

    Removed 'echo' test lines

--- lib/controller.php
+++ lib/controller.php
@@ -10,13 +10,11 @@
     public function loadModel($model)
     {
       $url = MODEL . $model . '.php';
-      // echo $url;
 
       if(file_exists($url)){
         require $url;
 
         $modelName = ucfirst($model) . 'Model';
-        // echo $modelName;
         $this->model = new $modelName;
       }
     }
