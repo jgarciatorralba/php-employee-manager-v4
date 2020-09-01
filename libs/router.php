833c2227b166e75dc8cd98136e09429bfbc36916
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-09-01 22:32:51 +0200

    Draft re-worked router with "/" instead of "&"

--- libs/router.php
+++ libs/router.php
@@ -10,31 +10,39 @@
 
     function __construct()
     {
-      if (!isset($_GET["controller"])){
-        $controller = new Controller();     // parent class (generic Controller)
+      $url = isset($_GET['url']) ? $_GET['url'] : null;
+      $url = rtrim($url, '/');
+      $url = explode('/', $url);
+
+      if (empty($url[0])){
+        $controller = new Controller();
+        
         if (SessionHelper::activeSession()) {
           $controller->view->render('dashboard');
         } else {
           $controller->view->render('login');
         }
       } else {
-        $controllerName = $_GET["controller"];
-        if (!file_exists(CONTROLLERS . $controllerName . ".php")) {
+
+        $controllerFile = CONTROLLERS . $url[0] . '.php';
+
+        if(!file_exists($controllerFile)){
           $controller = new Controller();
           $controller->view->render('error');
         } else {
-          include(CONTROLLERS . $controllerName . ".php");
-          $className = $controllerName . "Controller";
+          require_once $controllerFile;
+
+          $className = $url[0] . "Controller";
           $controller = new $className;
-          $controller->loadModel($controllerName);
+          $controller->loadModel($url[0]);
 
           $action;
-          if (isset($_REQUEST["action"])) {
-            $action = $_REQUEST["action"];
+          if(isset($url[1])){
+            $action = $url[1];
           } else {
-            if ($controllerName == "login"){
+            if ($url[0] == "login"){
               $action = "goToLogin";
-            } else if ($controllerName == "employee"){
+            } else if ($url[0] == "employee"){
               $action = "showEmployees";
             }
           }
@@ -45,5 +53,43 @@
         }
       }
     }
+
+    // function __construct()
+    // {
+    //   if (!isset($_GET["controller"])){
+    //     $controller = new Controller();     // parent class (generic Controller)
+    //     if (SessionHelper::activeSession()) {
+    //       $controller->view->render('dashboard');
+    //     } else {
+    //       $controller->view->render('login');
+    //     }
+    //   } else {
+    //     $controllerName = $_GET["controller"];
+    //     if (!file_exists(CONTROLLERS . $controllerName . ".php")) {
+    //       $controller = new Controller();
+    //       $controller->view->render('error');
+    //     } else {
+    //       include(CONTROLLERS . $controllerName . ".php");
+    //       $className = $controllerName . "Controller";
+    //       $controller = new $className;
+    //       $controller->loadModel($controllerName);
+
+    //       $action;
+    //       if (isset($_REQUEST["action"])) {
+    //         $action = $_REQUEST["action"];
+    //       } else {
+    //         if ($controllerName == "login"){
+    //           $action = "goToLogin";
+    //         } else if ($controllerName == "employee"){
+    //           $action = "showEmployees";
+    //         }
+    //       }
+
+    //       if (method_exists($controller, $action)) {
+    //         $controller->{$action}();
+    //       }
+    //     }
+    //   }
+    // }
 
   }
