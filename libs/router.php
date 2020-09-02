9f99f624b85c3f9d0a9b862c2b65bf57f37d2973
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-09-02 16:46:51 +0200

    Re-worked router to use "/" instead of "?" and "&"

--- libs/router.php
+++ libs/router.php
@@ -3,17 +3,23 @@
   /*
     This class will handle the routing of the web application, 
     switching between controllers, allowing views ("login", "dashboard" or "error" 
-    if none or an invalid controller is set), or executing controller actions.
+    if none or an invalid controller is set), or executing controller actions, 
+    with or without passed parameters.
   */
 
   class Router {
 
     function __construct()
     {
+      // Get parameters passed in the URL using "/"
       $url = isset($_GET['url']) ? $_GET['url'] : null;
       $url = rtrim($url, '/');
       $url = explode('/', $url);
 
+      /*
+        If no controller is passed in the URL, instantiate new generic controller 
+        to render a view depending on whether or not there is an active session.
+      */
       if (empty($url[0])){
         $controller = new Controller();
         
@@ -22,20 +28,26 @@
         } else {
           $controller->view->render('login');
         }
+      /*
+        If a controller is passed, check if it exists.
+      */
       } else {
-
         $controllerFile = CONTROLLERS . $url[0] . '.php';
-
+        // If it doesn't exist, instantiate generic controller to render error view.
         if(!file_exists($controllerFile)){
           $controller = new Controller();
           $controller->view->render('error');
+        // Otherwise, require controller file, then instantiate it and load model.
         } else {
           require_once $controllerFile;
-
           $className = $url[0] . "Controller";
           $controller = new $className;
           $controller->loadModel($url[0]);
 
+          /*
+            If an action is passed in the URL, store it in a variable. 
+            Otherwise store a default action for each controller.
+          */
           $action;
           if(isset($url[1])){
             $action = $url[1];
@@ -47,49 +59,25 @@
             }
           }
 
+          /*
+            Check if the associated method exists in the given controller. 
+            If additional parameters are passed in the URL, then store them in an array and call the method with the passed parameters, otherwise 
+            call the method without any parameter.
+          */
           if (method_exists($controller, $action)) {
-            $controller->{$action}();
+            $nParam = sizeof($url);
+            if ($nParam > 2){
+              $param = [];
+              for ($i = 2; $i<$nParam; $i++) {
+                array_push($param, $url[$i]);
+              }
+              $controller->{$action}($param);
+            } else {
+              $controller->{$action}();
+            }
           }
         }
       }
     }
-
-    // function __construct()
-    // {
-    //   if (!isset($_GET["controller"])){
-    //     $controller = new Controller();     // parent class (generic Controller)
-    //     if (SessionHelper::activeSession()) {
-    //       $controller->view->render('dashboard');
-    //     } else {
-    //       $controller->view->render('login');
-    //     }
-    //   } else {
-    //     $controllerName = $_GET["controller"];
-    //     if (!file_exists(CONTROLLERS . $controllerName . ".php")) {
-    //       $controller = new Controller();
-    //       $controller->view->render('error');
-    //     } else {
-    //       include(CONTROLLERS . $controllerName . ".php");
-    //       $className = $controllerName . "Controller";
-    //       $controller = new $className;
-    //       $controller->loadModel($controllerName);
-
-    //       $action;
-    //       if (isset($_REQUEST["action"])) {
-    //         $action = $_REQUEST["action"];
-    //       } else {
-    //         if ($controllerName == "login"){
-    //           $action = "goToLogin";
-    //         } else if ($controllerName == "employee"){
-    //           $action = "showEmployees";
-    //         }
-    //       }
-
-    //       if (method_exists($controller, $action)) {
-    //         $controller->{$action}();
-    //       }
-    //     }
-    //   }
-    // }
 
   }
