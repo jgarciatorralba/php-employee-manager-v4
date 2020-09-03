30bd16826b7de9621909a516c62ad5777714cb4a
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-09-03 13:52:09 +0200

    Error handling in DB request + msgs in session

--- models/login.php
+++ models/login.php
@@ -12,24 +12,39 @@
         public function checkCredentials($username, $password)
         {
             $user = $this->getUser($username);
+
+            if (gettype($user) == 'string'){
+                $_SESSION['loginError'] = $user;
+            } else {
+                if (!password_verify($password, $user["password"])) {
+                    $_SESSION['loginError'] = "Wrong username or password";
+                }
+            }
+
             return password_verify($password, $user["password"]);
         }
 
         public function getUser($username)
         {
-            $conn = $this->database->connect();
-            if ($conn) {
-                // Using prepared statements and named parameters (also positional params are an option)
-                $sql = 'SELECT * FROM users WHERE name = :name';
-                $stmt = $conn->prepare($sql);
-                $stmt->execute(['name' => $username]);
+            try {
+                $conn = $this->database->connect();
+                if (gettype($conn) != 'string') {
+                    // Using prepared statements and named parameters (also positional params are an option)
+                    $sql = 'SELECT * FROM users WHERE name = :name';
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute(['name' => $username]);
 
-                // set the resulting array to associative
-                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                $result = $stmt->fetch();
-                // close connection
-                $conn = null;
-                return $result;
+                    // set the resulting array to associative
+                    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                    $result = $stmt->fetch();
+                    // close connection
+                    $conn = null;
+                    return $result;
+                } else{
+                    return "There was a problem with the database connection";
+                }
+            } catch(PDOException $e){
+                return "There was a problem with the database query";
             }
         }
 
