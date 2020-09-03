e2d4125155e39cea77bae908d1b43962b9254eb4
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-09-03 23:23:03 +0200

    Added indentation, removed commented lines

--- views/login.php
+++ views/login.php
@@ -1,24 +1,32 @@
 <?php include "assets/head.php"; ?>
 <body class="d-flex flex-column min-vh-100">
-<?php include "assets/headerLogin.php"; ?>
+    <?php include "assets/headerLogin.php"; ?>
 
-<div class="container-sm m-auto">
-    <form class="form-signin text-center" action="<?php echo constant('URL'); ?>login/validateAccess" method="post" autocomplete="off">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputName" class="sr-only">Username</label>
-        <input type="text" id="inputName" class="form-control" name="username" placeholder="Username" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control mb-3" name="password" placeholder="Password" required>
-        <?php if (isset($_SESSION['loginError'])) { ?>
-            <div class="alert alert-danger" role="alert"><?php echo $_SESSION['loginError']; ?></div>
-        <?php } ?>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>
-</div>
+    <div class="container-sm m-auto">
+        <form class="form-signin text-center" action="<?php echo constant('URL'); ?>login/validateAccess" method="post" autocomplete="off">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputName" class="sr-only">Username</label>
+            <input type="text" id="inputName" class="form-control" name="username" placeholder="Username" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control mb-3" name="password" placeholder="Password" required>
 
-<?php
-    include "assets/footer.php";
-    include "assets/foot.php";
-?>
-<script src="<?php echo constant('URL'); ?>assets/js/login.js"></script>
+            <?php if(property_exists($this, 'message')) { ?>
+                <div id="error-login" class="alert alert-danger" role="alert"><?php echo $this->message; ?></div>
+            <?php } ?>
+            
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+
+    <?php
+        include "assets/footer.php";
+        include "assets/foot.php";
+    ?>
+    <script>
+        // Animation effect for the success message on submitting form
+        $('#error-login')
+            .fadeIn(800)
+            .delay(4000)
+            .fadeOut(800);
+    </script>
 </body>
