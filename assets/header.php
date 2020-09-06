b48db421c4838a2756d194807be8926934046542
jogarciatorralba <jjgarciatorralba@gmail.com>
2020-09-06 14:45:05 +0200

    Replaced Bootstrap navbar element

--- assets/header.php
+++ assets/header.php
@@ -1,35 +1,54 @@
-<header class="d-flex flex-row align-items-center bg-light border-bottom">
-    <nav class="navbar navbar-expand-sm mr-auto navbar-light">
-        <a class="navbar-brand">
-            <img src="<?php echo constant('URL'); ?>assets/img/assembler-school.jpg" class="rounded" width="30" height="30" alt="assembler school logo">
-        </a>
-        <a class="navbar-brand d-none d-sm-inline-block">Assembler School</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link font-weight-bold" id="dashboard" href="<?php echo constant('URL'); ?>login/goToDashboard">Dashboard</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" id="employee" href="<?php echo constant('URL'); ?>login/goToEmployee">Employee</a>
-                </li>
-            </ul>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+    <a class="navbar-brand" href="#">
+      <img
+        src="<?php echo constant('URL'); ?>assets/img/assembler-school.jpg"
+        width="30" height="30" class="rounded d-inline-block align-top mr-2 mt-0" alt="assembler school logo">
+      <div class="d-none d-sm-inline-block mt-0">
+        Assembler School
+      </div>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+      aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0 w-100 d-flex justify-content-between">
+        <div class="block-1 w-auto mt-0">
+          <li id="dashboard" class="nav-item active text-right d-lg-inline-block font-weight-bold">
+            <a class="nav-link" href="<?php echo constant('URL'); ?>login/goToDashboard">
+              Dashboard
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li id="employee" class="nav-item active text-right d-lg-inline-block">
+            <a class="nav-link" href="<?php echo constant('URL'); ?>login/goToEmployee">
+              Employee
+            </a>
+          </li>
         </div>
-    </nav>
-    <a class="text-primary text-decoration-none m-3" href="<?php echo constant('URL'); ?>login/kickout">Log out</a>
+        <div class="block-2 mt-0">
+          <li class="nav-item text-right">
+            <a class="nav-link text-primary" href="<?php echo constant('URL'); ?>login/kickout">
+              Logout
+            </a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </nav>
 </header>
 
 <script>
     const url = window.location.href;
-    if (url.includes('goToDashboard')) {
+    if (url.includes('Dashboard')) {
         document.getElementById('dashboard').classList.add('font-weight-bold');
         document.getElementById('employee').classList.remove('font-weight-bold');
     }
 
-    if (url.includes('goToEmployee')) {
+    if (url.includes('Employee')) {
         document.getElementById('employee').classList.add('font-weight-bold');
         document.getElementById('dashboard').classList.remove('font-weight-bold');
     }
